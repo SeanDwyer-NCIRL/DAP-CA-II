dcd21c8193c7c002a6252b325cdc608a6e4eed37
fionnstu <115401531+fionnstu@users.noreply.github.com>
2022-12-04 16:48:19 +0100

    Add files via upload

---
+++ consumer_spending.py
@@ -0,0 +1,130 @@
+######### Packages ########
+import pandas as pd
+import pandas.io.sql as sqlio
+import psycopg2
+from sodapy import Socrata
+import matplotlib as mpl
+
+########## Consumer Spending Percentage Change Downloaded and Converted to CSV ###########
+# This is updated weekly
+# See https://dev.socrata.com/foundry/data.ct.gov/xpjq-6wxn for details/code
+
+# For Header Description:
+# See https://data.ct.gov/Business/Percent-Change-in-Consumer-Spending-2020-2022/xpjq-6wxn
+
+# Public dataset aunthentication is not required, see None
+client = Socrata("data.ct.gov", None)
+
+# Returned as JSON from API / converted to Python list of dictionaries by sodapy.
+spend = client.get("xpjq-6wxn", limit=10000)
+
+# Convert to pandas DataFrame
+spend_df = pd.DataFrame.from_records(spend)
+
+# index=false as index column causes issues in postgres down the line
+spend_df.to_csv('consumer_spending.csv', index=False) 
+
+# Added new column id (1,2,3..) to csv, this will be the key in postgres
+spend_df = spend_df.assign(id = lambda x: range(1, 1 + len(spend_df)))
+
+# Edit colum headers, column all (causes syntax error in psycopg/postgres) changed to total.
+spend_df.columns =['statefips', 'date_day', 'total', 
+                     'acf', 'aer', 'apg', 'grf', 'hcs', 'tws', 
+                     'retail', 'retail_nogroc', 'id']
+					
+spend_df.to_csv("consumer_spending.csv", index=False)
+
+#################### Import CSV to PostgresSQL ####################
+# https://www.dataquest.io/blog/loading-data-into-postgres/
+
+# Connect to postgres and create database (cospdb)
+try:
+    conn = psycopg2.connect(database="postgres",
+                            user='postgres', password='fionn',
+                            host='localhost', port='5432')
+    conn.autocommit = True
+    cursor = conn.cursor()
+    cospdb = '''CREATE database cospdb;'''
+    cursor.execute(cospdb)
+    conn.close()
+except (Exception , psycopg2.Error) as dbError :
+    print ("Error:", dbError)
+finally:
+    if(conn):
+        conn.close()
+
+# Create Table
+cosp_table ="""
+    CREATE TABLE cosp(
+    statefips text,
+    date_day date,
+    total float,
+    acf float,
+    aer float,
+    apg float,
+    grf float,
+    hcs float,
+    tws float,
+    retail float,
+    retail_nogroc float,
+    id integer PRIMARY KEY
+)
+"""
+try:
+    conn = psycopg2.connect(database="postgres",
+                            user='postgres', password='fionn',
+                            host='localhost', port='5432')
+    cursor = conn.cursor()
+    cursor.execute(cosp_table)
+    conn.commit()
+except (Exception , psycopg2.Error) as dbError :
+    print ("Error:", dbError)
+finally:
+    if(conn):
+        conn.close()
+
+# insert csv data in to table
+try:
+    conn = psycopg2.connect(database="postgres",
+                            user='postgres', password='fionn',
+                            host='localhost', port='5432')
+    cursor = conn.cursor()
+    with open('consumer_spending.csv', 'r') as f:
+        next(f) 
+        cursor.copy_from(f, 'cosp', sep=',')
+    conn.commit()
+except (Exception , psycopg2.Error) as dbError :
+    print ("Error:", dbError)
+finally:
+    if(conn):
+        conn.close()
+
+############ Working on postgres database from python ############
+# Average % change per week grouped by week and year
+avg_week_year = """SELECT date_part('year', date_day::date) as year,
+       date_part('week', date_day::date) AS weekly,
+       AVG(total) as total, AVG(acf) as acf, AVG(aer) as aer,
+       AVG(apg) as apg, AVG(grf) as grf, AVG(hcs) as hcs,
+       AVG(tws) as tws, AVG(retail) as retail, AVG(retail_nogroc) as retail_nogroc           
+       FROM cosp
+       GROUP BY year, weekly
+       ORDER BY year, weekly;"""
+
+try:
+    conn = psycopg2.connect(database="postgres",
+                            user='postgres', password='fionn',
+                            host='localhost', port='5432')
+    cosp_df = sqlio.read_sql_query(avg_week_year, conn)
+    cosp_df.to_csv('all_avg_week_year.csv')
+except (Exception , psycopg2.Error) as dbError :
+    print ("Error:", dbError)
+finally:
+    if(conn):
+        conn.close()
+
+cosp_df.to_csv('all_avg_week_year.csv')
+
+
+
+
+
