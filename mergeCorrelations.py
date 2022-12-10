0acd61c3aa52802b3e922481daa45f15d347059c
donalpcollins <37088469+donalpcollins@users.noreply.github.com>
2022-12-10 13:16:05 +0000

    Update mergeCorrelations.py
    Added Sean's stock price dataframe into correlations

--- mergeCorrelations.py
+++ mergeCorrelations.py
@@ -1,13 +1,14 @@
-from datetime import datetime, timedelta
-
-#Add date column to cosp_df DataFrame
+from datetime import datetime
+#cosp_df
+#dfTwitterSentiment
+#changeDf
 cosp_df['date'] = pd.to_datetime(cosp_df.apply(lambda x: datetime.strptime('{0} {1} 1'.format(int(x['year']), int(x['weekly'])), '%Y %W %w'), axis=1))
-#Convert tweetdate column to datetime type 
 dfTwitterSentiment['tweetdate'] = pd.to_datetime(dfTwitterSentiment['tweetdate'])
-
-#Perform inner join merge on the 2 DataFrames and store results to combinedDf
 combinedDf = pd.merge(dfTwitterSentiment, cosp_df, left_on='tweetdate', right_on='date', how = 'inner')
+changeDf.reset_index(inplace=True)
+combinedDf2 = pd.merge(combinedDf, changeDf, left_on='tweetdate', right_on='Date', how = 'inner')
+#combinedDf2.head()
 
 #Run Pearson correlation on all columns of the DataFrame.
-correlationsDf = combinedDf.corr()
+correlationsDf = combinedDf2.corr()
 correlationsDf
